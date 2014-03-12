d73bfb6be32da6ec88d8720c60add6eeb6e48473
Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
2014-03-12 09:28:29 +0100

    fixed warning when autoloading namespaced class not in include path

--- app/code/local/Aoe/Generic/Model/Observer.php
+++ app/code/local/Aoe/Generic/Model/Observer.php
@@ -13,10 +13,16 @@ class Aoe_Generic_Model_Observer
         spl_autoload_register(function($class) {
             if (strrpos($class, '\\') !== false) {
                 // namespaced class name
-                $class = str_replace('\\', '_', ltrim($class, '\\'));
+                $fileName = str_replace('\\', DS, ltrim($class, '\\')) . '.php';
+                if (stream_resolve_include_path($fileName)) {
+                    $class = str_replace('\\', '_', ltrim($class, '\\'));
+                    return Varien_Autoload::instance()->autoload($class);
+                } else {
+                    return false;
+                }
+            } else {
+                return Varien_Autoload::instance()->autoload($class);
             }
-
-            return @Varien_Autoload::instance()->autoload($class);
         }, true, true);
     }
 }
